fa5ab695be416a5e849068095ffcc09515990d45
NightSea <sweetmilkshake0324@gmail.com>
2023-04-06 11:28:46 +0800

    feat: Add json

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from open_ai.open_ai import chat, messenge
-from flask import Flask, render_template, request
-import json
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -8,11 +7,16 @@ app = Flask(__name__)
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        msg = request.form["msg"]
-        chat("#zh-tw " + msg)
-        return json.dumps(messenge)
+        msg = request.json.get('msg')
+        gptResponse = chat("#zh-tw " + msg)
+        response = jsonify({'response': gptResponse})
+        response.headers['Content-Type'] = 'application/json'
+        return response
+
     return render_template("index.html", result=messenge)
 
 
-# start build server
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.debug = True
+    # start build server
+    app.run(host='0.0.0.0', port=8080)
